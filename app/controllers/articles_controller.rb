5350279833c85667c20bfd1c49236d7965a175e1
Magi <magi@magi.com>
2018-12-01 15:32:11 -0200

    Add bg, style. start footer.

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -11,49 +11,21 @@ class ArticlesController < ApplicationController
   def edit
   end
 
+
   def create
     @article = Article.new(article_params)
     if @article.save
-      flash[:notice] = "Article was succesfully posted!"
+      flash[:success] = "Article was succesfully posted!"
       redirect_to article_path(@article)
     else
       render 'new'
     end
   end
 
-  def create
-    @article = Article.new(article_params)
-    if @article.save
-      flash[:notice] = "Article was succesfully posted!"
-      redirect_to article_path(@article)
-    else
-      render 'new'
-    end
-  end
-
-  def create
-    @article = Article.new(article_params)
-    if @article.save
-      flash[:notice] = "Article was succesfully posted!"
-      redirect_to article_path(@article)
-    else
-      render 'new'
-    end
-  end
-
-  def create
-    @article = Article.new(article_params)
-    if @article.save
-      flash[:notice] = "Article was succesfully posted!"
-      redirect_to article_path(@article)
-    else
-      render 'new'
-    end
-  end
 
   def update
     if @article.update(article_params)
-      flash[:notice] = "Article was successfully updated."
+      flash[:success] = "Article was successfully updated."
       redirect_to article_path(@article)
     else
       render 'edit'
@@ -65,7 +37,7 @@ class ArticlesController < ApplicationController
 
   def destroy
     @article.destroy
-    flash[:notice] = "Article was successfully deleted."
+    flash[:danger] = "Article was successfully deleted."
     redirect_to articles_path
   end
   private
