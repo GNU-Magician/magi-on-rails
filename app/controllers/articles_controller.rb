693be8dbd82ea99b86183e2471e79c4dd73f994c
Magi <magi@magi.com>
2018-12-03 14:58:48 -0200

    Add association between users and articles. Add debugger.

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -14,6 +14,8 @@ class ArticlesController < ApplicationController
 
   def create
     @article = Article.new(article_params)
+    @article.user = User.first
+
     if @article.save
       flash[:success] = "Article was succesfully posted!"
       redirect_to article_path(@article)
