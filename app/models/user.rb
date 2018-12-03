479e692ff4f5dcfbb58117bbeaa32f063c724f82
Magi <magi@magi.com>
2018-12-03 14:24:06 -0200

    Add in user validation.

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,3 +1,16 @@
 class User < ApplicationRecord
+  validates :username,
+            presence: true,
+            uniqueness: { case_sensitive: false},
+            length: 5..20
+
+
+  VALID_EMAIL_REGEX = /(^[a-zA-Z0-9_.+-]+@[a-zA-Z0-9-]+\.[a-zA-Z0-9-.]+$)/i
+  validates :email,
+            presence: true,
+            length: 8..50,
+            uniqueness: { case_sensitive: false },
+            format: { with: VALID_EMAIL_REGEX }
+
 
 end
