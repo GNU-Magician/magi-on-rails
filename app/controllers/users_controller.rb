c5c6ddb48511103428d8a28e178ed1f4e9ed870e
GNU-Magician <31811114+gnu-magician@users.noreply.github.com>
2018-12-04 21:57:53 -0200

    added user functionality, not complete tho

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,5 +1,5 @@
 class UsersController < ApplicationController
-
+  before_action :set_user, only: [:edit, :update, :show]
 
   def index
     @users = User.paginate(page: params[:page], per_page: 5)
@@ -22,11 +22,9 @@ class UsersController < ApplicationController
   end
 
   def edit
-    @user = User.find(params[:id])
   end
 
   def update
-    @user = User.find(params[:id])
     if @user.update(user_params)
        flash[:success] = "Your account was updated succesfully."
        redirect_to articles_path
@@ -36,7 +34,6 @@ class UsersController < ApplicationController
   end
 
   def show
-    @user = User.find(params[:id])
     @user_articles = @user.articles.paginate(page: params[:page], per_page: 5)
 
   end
@@ -44,6 +41,10 @@ class UsersController < ApplicationController
   private
   def user_params
     params.require(:user).permit(:username, :email, :password)
+  end
+
+  def set_user
+    @user = User.find(params[:id])
   end
 end
 
